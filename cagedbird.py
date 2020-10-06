dc10933163f0be6e5568692b186c0421d9d0726f
lotsofluck <anitabangali@yahoo.com>
2020-10-06 01:17:12 -0500

    Update cagedbird.py

--- cagedbird.py
+++ cagedbird.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import sample
 
 caged_bird = "poem.txt"
 infile = open("poem.txt", "r")
@@ -6,15 +6,28 @@ infile.readlines(caged_bird)
 
 def get_file_lines(caged_bird):
     lines_list = ["caged_bird"]
-    return line_list
+    return lines_list
 
 def lines_printed_backward(caged_bird):
     lines_list = ["caged_bird"]
     return lines_list[::,-1]
 
 def lines_printed_random(caged_bird):
+    lines_list = "poem.txt" 
+    lines = open(lines_list, "r").read().splitlines()
+
+    for i in sample(lines, i=len(lines)):
+        return lines
+
+def lines_printed_custom(caged_bird):
     lines_list = ["caged_bird"]
     return lines_list
 
-def lines_printed_custom():
-    
+
+print(get_file_lines(caged_bird))
+
+print(lines_printed_backward)
+
+print(lines_printed_random)
+
+print(lines_printed_custom[])
